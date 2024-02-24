a5c66756f6617b8c72b1222e1dd15112209139f6
atPalash <mpalash.halder@gmail.com>
2024-02-24 10:38:06 +0000

    Remove user login and authentication for now
    Signed-off-by: atPalash <mpalash.halder@gmail.com>

--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoute = () => {
-  const isAuthenticated = localStorage.getItem("token") ? true : false;
+  const isAuthenticated = true //localStorage.getItem("token") ? true : false;
 
   return isAuthenticated ? <Navigate to="/" /> : <Outlet />;
 };
